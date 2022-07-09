1bcac9c9fbe3b67ff4944c4b3da10f71178e4244
Fernando B <fernandobe+git@protonmail.com>
2022-07-09 17:28:56 -0500

    fix-export

--- callbacks.h
+++ callbacks.h
@@ -16,9 +16,9 @@ extern "C" {
     #endif
 
     #ifdef LIBPLCTAGDLL_EXPORTS
-        #define LIB_EXPORT __declspec(dllimport)
-    #else
-        #define LIB_EXPORT __declspec(dllexport)        
+        #define LIB_EXPORT __declspec(dllexport)
+    #else        
+        #define LIB_EXPORT __declspec(dllimport)        
     #endif
 #else
     #ifdef LIBPLCTAGDLL_EXPORTS
@@ -30,15 +30,15 @@ extern "C" {
 
 // int32_t go_plc_tag_create_ex(const char *attrib_str, void (*tag_callback_func)(int32_t tag_id, int event, int status, void *userdata), void *userdata, int timeout);
 typedef void (*go_plc_tag_create_ex_callback)(int32_t tag_id, int event, int status, void *userdata);
-LIB_EXPORT int32_t go_plc_tag_create_ex(const char *attrib_str, go_plc_tag_create_ex_callback callback_function, void *userdata, int timeout);
+int32_t go_plc_tag_create_ex(const char *attrib_str, go_plc_tag_create_ex_callback callback_function, void *userdata, int timeout);
 
 // int plc_tag_register_callback_ex(int32_t tag_id, void (*tag_callback_func)(int32_t tag_id, int event, int status, void *userdata), void *userdata);
 typedef void (*go_plc_tag_register_ex_callback)(int32_t tag_id, int event, int status, void *userdata);
-LIB_EXPORT int go_plc_tag_register_callback_ex(int32_t tag_id, go_plc_tag_register_ex_callback callback_function, void *userdata);
+int go_plc_tag_register_callback_ex(int32_t tag_id, go_plc_tag_register_ex_callback callback_function, void *userdata);
 
 // int plc_tag_register_logger(void (*log_callback_func)(int32_t tag_id, int debug_level, const char *message));
 typedef void (*go_plc_tag_register_logger_callback)(int32_t tag_id, int debug_level, const char *message);
-LIB_EXPORT int go_plc_tag_register_logger(go_plc_tag_register_logger_callback callback_function);
+int go_plc_tag_register_logger(go_plc_tag_register_logger_callback callback_function);
 
 #ifdef __cplusplus
 }
